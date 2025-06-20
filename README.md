bdce88a058f380ca7d7f40fda2aefc5728a340ce
Chea Ritheavatey <2023447chea@aupp.edu.kh>
2025-06-20 10:15:19 +0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # javascriptLearning
+
+1. data type and variable
+- undefined = not set to anything
+- null = set to be sth and that sth is "nothing"
+- boolean = true or false
+- string = word or text 
+- symbol = primitive value that is unique
+- number = lek
+- object = store key-value pair
+
+and we set data into variable
+
+- case sensitive
---
+++ index.js
@@ -0,0 +1,47 @@
+// data type and variable and we use 'var' = variable
+var name = "Vatey"
+name = 8
+// another way to make variable is 'let' and 'const'
+let ourname = "ourname"
+const pi = 3.14
+
+// whats the diffeence?
+/* var = can be used throughout the whole program
+    let = can be use only in the scope you defined
+    const = variable should never change. just like final keyword in java
+    */
+
+// declaring a variable
+var a;
+
+// declare and assign to a vale
+var b = 2;
+
+a = 7;
+
+b = a;
+
+// print
+console.log(b);
+
+// initalize variable to value when declare
+var a = 9;
+
+// + - * /
+var sum = 10.4 + 90;
+var difference = 80 - 50;
+var product = 8 * 5;
+var quotient = 66 / 9;
+var remainder = 10.3 % 3;
+
+console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(quotient);
+console.log(remainder);
+
+// increment
+sum++; 
+
+// decrement
+sum--;
